c1a4afe491235d4a314a5fbc3f46046559cedbf6
dechariot <nsprisang@gmail.com>
2020-03-22 11:47:56 +0700

    upload file to Github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Card, Button } from 'react-bootstrap'
 
-function App() {
+export default function App() {
   return (
+    //Start Website
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Movie Card */}
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the bulk of
+            the card's content.</Card.Text>
+          <Button variant="secondary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+      {/* Movie Card */}
+
+
     </div>
+    // End Website
   );
 }
 
-export default App;
